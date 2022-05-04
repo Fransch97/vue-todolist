ce78a46ebe8d25aee8c7d739a931d3a6d85cd888
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-04 16:07:29 +0200

    first vue scripts

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Yes master")
+console.log("Yes master");
+
+const app = new Vue({
+    el: "#app",
+    data:{
+        todoLists:[{
+            todo: "buy bananas",
+            done: false
+        }],
+        toAddTodo:{
+            todo: "",
+            done:false
+        }
+    },
+
+    methods:{
+
+    },
+
+    mounted() {
+        console.log("Working")
+    },
+})
